68c624e564edda3954b1fb653bb780f582cfba29
h_hoshino <h_hoshino@kakehashi-skysol.co.jp>
2018-07-03 19:02:39 +0900

    mod:

--- twitter_tweet.py
+++ twitter_tweet.py
@@ -2,23 +2,53 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
  
-import tweepy
 import settings
-import sys
+from requests_oauthlib import OAuth1Session
+import json
 
 # key
-CONSUMER_KEY = settings.consumer_key
-CONSUMER_SECRET = settings.consumer_secret
-ACCESS_TOKEN = settings.token
-ACCESS_SECRET = settings.token_secret
+CK = settings.consumer_key
+CS = settings.consumer_secret
+AT = settings.token
+AS = settings.token_secret
+# ツイート投稿用のURL
+url_media = "https://upload.twitter.com/1.1/media/upload.json"
+url_text = "https://api.twitter.com/1.1/statuses/update.json"
+# OAuth認証で POST method で投稿
+twitter = OAuth1Session(CK, CS, AT, AS)
 
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
- 
-api = tweepy.API(auth)
-argvs = sys.argv  # コマンドライン引数を格納したリストの取得
+def postTweetWithPicture():
+    # ツイート本文
+    files = {"media" : open('1.jpg', 'rb')}
+    req_media = twitter.post(url_media, files = files)
 
-text = "I'm playing " +argvs[1] + " with " + argvs[2]
-api.update_with_media(filename='./85.jpg',status=text)
+    # レスポンスを確認
+    if req_media.status_code != 200:
+        print ("画像アップデート失敗: %s", req_media.text)
+        exit()
 
-print('Done!')
+    # Media ID を取得
+    media_id = json.loads(req_media.text)['media_id']
+    print ("Media ID: %d" % media_id)
+
+    # Media ID を付加してテキストを投稿
+    params = {'status': '画像投稿テスト', "media_ids": [media_id]}
+    req_media = twitter.post(url_text, params = params)
+
+    # 再びレスポンスを確認
+    if req_media.status_code != 200:
+        print ("テキストアップデート失敗: %s", req_text.text)
+        exit()
+
+    print ("OK")
+
+def postTweetText():
+    # ツイート本文
+    params = {"status": "Hello, World!"}
+    req = twitter.post(url_text, params = params)
+
+    # レスポンスを確認
+    if req.status_code == 200:
+        print ("OK")
+    else:
+        print ("Error: %d" % req.status_code)
