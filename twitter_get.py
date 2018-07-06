aef6099e0c105f11f49690d273af450ddfe3352a
h_hoshino <h_hoshino@kakehashi-skysol.co.jp>
2018-07-06 09:49:50 +0900

    add:get

--- twitter_get.py
+++ twitter_get.py
@@ -1,24 +1,65 @@
 
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
- 
-import tweepy
+
+from requests_oauthlib import OAuth1Session
+import json
 import settings
 import sys
 
-# key
-CONSUMER_KEY = settings.consumer_key
-CONSUMER_SECRET = settings.consumer_secret
-ACCESS_TOKEN = settings.token
-ACCESS_SECRET = settings.token_secret
+### Constants                                                                                                                                                     
+oath_key_dict = {
+    "consumer_key": settings.consumer_key,
+    "consumer_secret": settings.consumer_secret,
+    "access_token": settings.token,
+    "access_token_secret": settings.token_secret
+}
+### Functions                                                                                                                                                     
+def main():
+    tweets = tweet_search("#python", oath_key_dict)
+    for tweet in tweets["statuses"]:
+        tweet_id = tweet[u'id_str']
+        text = tweet[u'text']
+        created_at = tweet[u'created_at']
+        user_id = tweet[u'user'][u'id_str']
+        user_description = tweet[u'user'][u'description']
+        screen_name = tweet[u'user'][u'screen_name']
+        user_name = tweet[u'user'][u'name']
+        print "tweet_id:", tweet_id
+        print "text:", text
+        print "created_at:", created_at
+        print "user_id:", user_id
+        print "user_desc:", user_description
+        print "screen_name:", screen_name
+        print "user_name:", user_name
+    return
 
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
- 
-api = tweepy.API(auth)
-argvs = sys.argv  # コマンドライン引数を格納したリストの取得
 
-text = "I'm playing " +argvs[1] + " with " + argvs[2]
-api.update_with_media(filename='./85.jpg',status=text)
+def create_oath_session(oath_key_dict):
+    oath = OAuth1Session(
+    oath_key_dict["consumer_key"],
+    oath_key_dict["consumer_secret"],
+    oath_key_dict["access_token"],
+    oath_key_dict["access_token_secret"]
+    )
+    return oath
 
-print('Done!')
+def tweet_search(search_word, oath_key_dict):
+    url = "https://api.twitter.com/1.1/search/tweets.json?"
+    params = {
+        "q": unicode(search_word),
+        "lang": "ja",
+        "result_type": "recent",
+        "count": "15"
+        }
+    oath = create_oath_session(oath_key_dict)
+    responce = oath.get(url, params = params)
+    if responce.status_code != 200:
+        print "Error code: %d" %(responce.status_code)
+        return None
+    tweets = json.loads(responce.text)
+    return tweets
+
+### Execute                                                                                                                                                       
+if __name__ == "__main__":
+    main()
